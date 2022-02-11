a14b4c69e5127461fc360475b952a255b9f29578
guilhermeMgimenez <94083701+guilhermeMgimenez@users.noreply.github.com>
2022-02-10 22:23:34 -0300

    começo atividade 17

---
+++ 017/script.js
@@ -0,0 +1,11 @@
+let valor1
+let valor2
+valor1 = 12
+valor2 = 34
+console.log("digite um número")
+//adição de dado
+console.log("digite um número")
+//adição de dado
+if(valor1 == valor2){
+    console.log(nume)
+}
