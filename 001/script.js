06ba9c83dcadd7ee8346e07c37198379e7615a91
guilhermeMgimenez <94083701+guilhermeMgimenez@users.noreply.github.com>
2022-03-03 20:15:45 -0300

    criação interface javascript

--- 001/script.js
+++ 001/script.js
@@ -1,5 +1,10 @@
 function alo(){
-console.log("alo mundo")
+    document.querySelector("#resultado").value = "alo mundo"
+    //saida de dados no corpo do navegador:
+    //document.write("alo mundo")
+    //em uma div, span, h1, etc:
+    //document.querySelector("#resultado").innerHTML = "alo mundo"
+    //em um campo de texto:
+    //document.querySelector("#resultado").value = "alo mundo"
 }
-alo()
 
